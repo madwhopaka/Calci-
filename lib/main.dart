ca3afe2156e38fa88d3e9a3c9ffd7db098b08135
madwho <madhupaka11@gmail.com>
2021-08-19 13:31:48 +0530

    done but calcul function is left

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,207 @@
 import 'package:flutter/material.dart';
-import './widgets/calcbtn.dart'; 
-
+import './widgets/calcbtn.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _hist = '';
+  String _exp = '';
+  void ac(String text) {
+    setState(() {
+      _hist = '';
+      _exp = '';
+    });
+  }
+
+  void clear(String text) {
+    setState(() {
+      if (_exp != '') {
+        _exp = _exp.substring(0, _exp.length - 1);
+      }
+    });
+  }
+
+  void numClick(String text) {
+    setState(() {
+      _exp += text;
+    });
+  }
+
+  void calculate(String text) {
+    setState(() {
+      if (_exp != '') {
+        _hist = _exp;
+        _exp = "solved";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: "Calci",
       home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("Humara Calci")),
-        ),
-        backgroundColor: const Color(0xFF28367),
-        body: Center(child: CalcBtn(text:'1')),
+        backgroundColor: const Color(0xFF283637),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Container(
+              alignment: Alignment(1, 1),
+              padding: EdgeInsets.only(right: 12),
+              child: Text(
+                _hist,
+                style: GoogleFonts.rubik(
+                    textStyle: TextStyle(color: Colors.white30, fontSize: 24)),
+              ),
+            ),
+            Container(
+              alignment: Alignment(1, 1),
+              padding: EdgeInsets.all(12),
+              child: Text(
+                _exp,
+                style: GoogleFonts.rubik(
+                    textStyle: TextStyle(color: Colors.white, fontSize: 50)),
+              ),
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  CalcBtn(
+                      callback: ac,
+                      text: 'AC',
+                      fillColor: 0xFF6C807F,
+                      textSize: 18),
+                  CalcBtn(
+                      callback: clear,
+                      text: 'C',
+                      fillColor: 0xFF6C807F,
+                      textSize: 24),
+                  CalcBtn(
+                      callback: numClick,
+                      text: '%',
+                      fillColor: 0xFFFFFFFF,
+                      textColor: 0xFF65BDAC,
+                      textSize: 24),
+                  CalcBtn(
+                      callback: numClick,
+                      text: '/',
+                      fillColor: 0xFFFFFFFF,
+                      textColor: 0xFF65BDAC,
+                      textSize: 24)
+                ]),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  CalcBtn(
+                      callback: numClick,
+                      text: '1',
+                      fillColor: 0000000000,
+                      textSize: 24),
+                  CalcBtn(
+                      callback: numClick,
+                      text: '2',
+                      fillColor: 0000000000,
+                      textSize: 24),
+                  CalcBtn(
+                      callback: numClick,
+                      text: '3',
+                      fillColor: 0000000000,
+                      textSize: 24),
+                  CalcBtn(
+                      callback: numClick,
+                      text: ' x ',
+                      fillColor: 0xFFFFFFFF,
+                      textColor: 0xFF65BDAC,
+                      textSize: 24)
+                ]),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  CalcBtn(
+                      callback: numClick,
+                      text: '4',
+                      fillColor: 0000000000,
+                      textSize: 24),
+                  CalcBtn(
+                      callback: numClick,
+                      text: '5',
+                      fillColor: 0000000000,
+                      textSize: 24),
+                  CalcBtn(
+                      callback: numClick,
+                      text: '6',
+                      fillColor: 0000000000,
+                      textSize: 24),
+                  CalcBtn(
+                      callback: numClick,
+                      text: '-',
+                      fillColor: 0xFFFFFFFF,
+                      textColor: 0xFF65BDAC,
+                      textSize: 24)
+                ]),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  CalcBtn(
+                      callback: numClick,
+                      text: '1',
+                      fillColor: 0000000000,
+                      textSize: 24),
+                  CalcBtn(
+                      callback: numClick,
+                      text: '2',
+                      fillColor: 0000000000,
+                      textSize: 24),
+                  CalcBtn(
+                      callback: numClick,
+                      text: '3',
+                      fillColor: 0000000000,
+                      textSize: 24),
+                  CalcBtn(
+                      callback: numClick,
+                      text: '+',
+                      fillColor: 0xFFFFFFFF,
+                      textColor: 0xFF65BDAC,
+                      textSize: 24)
+                ]),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  CalcBtn(
+                      callback: numClick,
+                      text: '.',
+                      fillColor: 0000000000,
+                      textSize: 24),
+                  CalcBtn(
+                      callback: numClick,
+                      text: '0',
+                      fillColor: 0000000000,
+                      textSize: 24),
+                  CalcBtn(
+                      callback: numClick,
+                      text: '00',
+                      fillColor: 0000000000,
+                      textSize: 24),
+                  CalcBtn(
+                      callback: calculate,
+                      text: '=',
+                      fillColor: 0xFFFFFFFF,
+                      textColor: 0xFF65BDAC,
+                      textSize: 24)
+                ])
+          ],
+        )),
       ),
     );
   }
--- lib/widgets/calcbtn.dart
+++ lib/widgets/calcbtn.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CalcBtn extends StatelessWidget {
   final String text;
-  const CalcBtn({Key? key, required this.text}) : super(key: key);
+  final int textColor;
+  final int fillColor;
+  final Function callback;
+  final double textSize;
+  const CalcBtn(
+      {Key? key,
+      required this.text,
+      required this.fillColor,
+      required this.textSize,
+      required this.callback,
+      this.textColor = 0xFFFFFFFF})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 60,
-      width: 60,
-      child: OutlineButton(child: Text(this.text),onPressed:(){})); 
+    return Container(
+        margin: EdgeInsets.all(8),
+        child: SizedBox(
+            height: 60,
+            width: 60,
+            child: ElevatedButton(
+              child: Text(this.text,
+                  style: GoogleFonts.rubik(
+                      textStyle: TextStyle(
+                          fontSize: textSize, color: Color(textColor)))),
+              onPressed: () {
+                callback(text);
+              },
+              style: ButtonStyle(
+                  elevation: MaterialStateProperty.all(0),
+                  backgroundColor: fillColor != null
+                      ? MaterialStateProperty.all<Color>(Color(fillColor))
+                      : null,
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ))),
+            )));
   }
 }
